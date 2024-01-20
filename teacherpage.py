f5c7c598c41a56f2fd0deed385508171b5142d02
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 16:47:05 -0500

    Update teacherpage.py

--- teacherpage.py
+++ teacherpage.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
+'''
+Content
+
+1. communication outside of speaking (how to email teachers, ask for extensions, etc)
+2. asking questions in class
+3. misc (asking how their day is idk)
+
+'''
+
 #page the school setting button will redirect to
