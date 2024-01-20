8e8df6baa48114e811edd4977c80b13d9b4939df
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:13:36 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -3,12 +3,12 @@ import streamlit as st
 #This will be the page that will be redirected when the interact with a peer/friend button is clicked
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
-with st.chat_message("user"):
+with st.chat_message("ai"):
     st.write("Hello 👋")
     st.write("Let us learn how to communicate effectively with peers and friends!")
 
+with st.chat_message("user"):
     inp = st.chat_input('Type to continue: ')
-
     if inp:
         if st.button('Learn to strike up a conversation'):
             st.write('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
