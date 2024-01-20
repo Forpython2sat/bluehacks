e175f8113f61e7ee9e0580c53b928d3028e5c91c
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:12:35 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -7,11 +7,10 @@ with st.chat_message("user"):
     st.write("Hello 👋")
     st.write("Let us learn how to communicate effectively with peers and friends!")
 
-inp = st.chat_input('Type to continue: ')
+    inp = st.chat_input('Type to continue: ')
 
-if inp:
-    if st.button('Learn to strike up a conversation'):
-        with st.chat_message("user"):
+    if inp:
+        if st.button('Learn to strike up a conversation'):
             st.write('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
             st.write("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
             st.write("Therefore, if you know that something has been on their minds, talk to them about it.")
