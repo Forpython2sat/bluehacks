e03fff0bdd55be41c342d0a867b3efd563d8893f
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 14:19:27 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -4,5 +4,4 @@ st.write("""
 Breaking Barriers - Blues Project Idea
 """)
 
-st.link_button("Learn to Interact in a Classroom Setting", "https://dmoj.ca/user/laight")
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
