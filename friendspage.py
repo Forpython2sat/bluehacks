b851a571b254a44165907f054037bddfba5f4c3f
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:19:14 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -1,19 +1,21 @@
 import streamlit as st
 
-#This will be the page that will be redirected when the interact with a peer/friend button is clicked
-animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+def userout(s):
+    with st.chat_message("user"):
+        st.write(s)
 
-with st.chat_message("ai"):
-    st.write("Hello 👋")
-    st.write("Let us learn how to communicate effectively with peers and friends!")
+def aiout(s):
+    with st.chat_message('ai'):
+        st.write(s)
 
-inp = st.chat_input('Type to continue: ')
+aiout("Hello 👋")
+aiout("Let us learn how to communicate effectively with peers and friends!")
+
+inp = st.chat_input('Say something: ')
 
 if inp:
-    with st.chat_message("user"):
-        st.write(inp)
-    with st.chat_message("user"):
-        if st.button('Learn to strike up a conversation'):
-            st.write('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
-            st.write("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
-            st.write("Therefore, if you know that something has been on their minds, talk to them about it.")
+    userout(inp)
+    if st.button('Learn to strike up a conversation'):
+        aiout('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
+        aiout("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
+        aiout("Therefore, if you know that something has been on their minds, talk to them about it.")
