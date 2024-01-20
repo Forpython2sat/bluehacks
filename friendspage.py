f4d6617dc52b7ec4c93d1e4aa2817e7d909dca38
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 18:31:56 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 outl = ['We will first learn how to open conversations. Opening conversations are sometimes quite challenging, and often times, the conversation halts after an exchange of greetings. What would you do to prevent this from happening?', 'Usually, we should talk about something that they are interested in. Psychology shows that humans tend to engage more when something is about them, or if something interests them. Thus, next time you see your friend, ask him whats going good for him, what may be troubling him, and also ask him about his hobbies.']
 
-user = st.chat_message('user')
+def counterplus():
+    global counter
+    counter += 1
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -16,14 +18,13 @@ for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
-prompt = st.chat_input('Say something')
-counter = -1
+prompt = st.chat_input(placeholder='Say something', on_submit=counterplus)
+counter = 0
 if prompt:
-    counter += 1
     with st.chat_message("user"):
         st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt, 'counter': counter})
-    response = prompt
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    response = outl[counter]
     with st.chat_message('ai'):
         st.markdown(response)
-    st.session_state.messages.append({'role': 'ai', 'content': response, 'counter': counter})
+    st.session_state.messages.append({'role': 'ai', 'content': response})
