7e0014f9e9e0abb756c165e3c747466d97a3b8f6
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 14:15:36 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -6,5 +6,4 @@ Breaking Barriers - Blues Project Idea
 """)
 
 url = "https://dmoj.ca/user/laight"
-if st.button('Learn to Interact in a classroom setting'):
- webbrowser.open_new_tab(url)
+st.link_button("Learn to Interact in a Classroom Setting", url)
