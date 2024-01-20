c8099bcc7da00fbbd7d0ccc010141168e328dfa5
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 18:08:21 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -3,7 +3,7 @@ import streamlit as st
 outl = ['We will first learn how to open conversations. Opening conversations are sometimes quite challenging, and often times, the conversation halts after an exchange of greetings. What would you do to prevent this from happening?', 'Usually, we should talk about something that they are interested in. Psychology shows that humans tend to engage more when something is about them, or if something interests them. Thus, next time you see your friend, ask him whats going good for him, what may be troubling him, and also ask him about his hobbies.']
 
 if "messages" not in st.session_state:
-    st.sessions_state.messages = []
+    st.session_state.messages = []
 
 with st.chat_message('ai'):
     st.write('"Hello 👋"')
