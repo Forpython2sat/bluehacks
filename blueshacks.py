da56d7ae01a474e45c0940519ce50e5c0edc0c5f
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 14:12:33 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import pandas as pd
+import webbrowser
  
 st.write("""
 Breaking Barriers - Blues Project Idea
 """)
 
+url = 'https://dmoj.ca'
+
 school = st.button('Learn to Interact in a classroom setting')
+if school:
+ webbrowser.open_new_tab(url)
