1229be79c146a6a338b0aab7e9a3b82aa8c6ee7c
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 18:51:44 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -2,10 +2,11 @@ import streamlit as st
  
 st.write("""
 Breaking Barriers - Blues Project Idea
+pee
 """)
 
 htp="https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
-st.image(htp, caption= 'logo', width=350)
+st.image(htp, caption= "joe biden", width=350)
 
 st.link_button("dmoj link", "https://dmoj.ca/user/laight")
 st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
