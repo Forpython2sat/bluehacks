c605933977e8a5715506f9b4f05260cdf5eca608
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:30:08 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -13,11 +13,14 @@ ai.write("Hello 👋")
 ai.write("Let us learn how to communicate effectively with peers and friends!")
 ai.write("Click any button to get started")
 
-if st.button('Learn to strike up a conversation', on_click=click_button):
+st.button('Learn to strike up a conversation', on_click=click_button)
+
+if st.session_state.clicked:
     ai.write('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
     ai.write("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
     ai.write("Therefore, if you know that something has been on their minds, talk to them about it.")
-if st.button('Learn to elegantly decline', on_click=click_button):
+st.button('Learn to elegantly decline', on_click=click_button)
+if st.session_state.clicked:
     ai.write("Often our friends can unkowningly ask us something that we are uncomfortable with.")
     ai.write("Therefore, it is essential to know how to decline.")
     ai.write("Usually, it is best to be up front about it, and hold your ground despite the pressure.")
