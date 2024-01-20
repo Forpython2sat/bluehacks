62c4464048b8f3436cd9d5ab6e395b33b5145c73
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 18:48:00 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -4,6 +4,8 @@ st.write("""
 Breaking Barriers - Blues Project Idea
 """)
 
+st.image('teacher.png', caption='Sunrise by the mountains')
+
 st.link_button("dmoj link", "https://dmoj.ca/user/laight")
 st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
 st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
