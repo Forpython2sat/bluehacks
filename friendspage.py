6b5a59dc5d28b8a948d593eb3c2fed71123f96b7
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:04:12 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
 #This will be the page that will be redirected when the interact with a peer/friend button is clicked
+
+import streamlit as st
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
+
 with st.chat_message("user"):
     st.write("Hello 👋")
 with st.chat_message("user"):
