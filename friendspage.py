5aa37f290f3a1178f33d0956b0b9b347201149f3
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 15:09:43 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -3,3 +3,8 @@ import streamlit as st
 #This will be the page that will be redirected when the interact with a peer/friend button is clicked
 with st.chat_message("user"):
     st.write("Hello 👋")
+with st.chat_message("user"):
+    st.write("Let us learn how to communicate effectively with peers and friends!")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
