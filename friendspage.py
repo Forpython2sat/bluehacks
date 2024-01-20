73fb50e028ee9dbcbbd396f98640465ab29f9fc4
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 17:22:27 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -1,21 +1,17 @@
 import streamlit as st
 
-def userout(s):
-    with st.chat_message("user"):
-        st.write(s)
+ai = st.chat_message('ai')
+user = st.chat_message('user')
 
-def aiout(s):
-    with st.chat_message('ai'):
-        st.write(s)
-
-aiout("Hello 👋")
-aiout("Let us learn how to communicate effectively with peers and friends!")
+ai.write("Hello 👋")
+ai.write("Let us learn how to communicate effectively with peers and friends!")
 
 inp = st.chat_input('Say something: ')
 
 if inp:
-    userout(inp)
-    if st.button('Learn to strike up a conversation'):
-        aiout('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
-        aiout("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
-        aiout("Therefore, if you know that something has been on their minds, talk to them about it.")
+    user.write(inp)
+
+if st.button('Learn to strike up a conversation'):
+    ai.write('''Often, our conversations are cut short after a simple "hello," and we are launched into a state of awkwardness.''')
+    ai.write("According to psychology, however, people typically tend to engage more when the conversation is something that concerns them or interests them.")
+    ai.write("Therefore, if you know that something has been on their minds, talk to them about it.")
