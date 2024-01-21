0340c7ff53878a9fe3644af888dcc1ee7e8c841c
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 19:25:42 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 col1, col2 = st.columns(2)
-col1.write("this is column 1")
-col2.write("this is column 2")
 
 with col1:
   st.header('Interact in a Classroom Environment')
   st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
-with col1:
+with col2:
   st.header('Interact Smoother with Your Peers')
   st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
 
