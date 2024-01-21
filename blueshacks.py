b38d74de3c75444def5463343427490c25d4ef24
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 19:13:46 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -1,17 +1,17 @@
 import streamlit as st
-import streamlit_js_eval
-import json
 
-st.write(f"Screen width is _{streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}_")
+col1, col2 = st.columns(2)
+col1.write("this is column 1")
+col2.write("this is column 2")
 
-st.write("""
+col1.write("""
 joe biden
 """)
 
 htp="https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
-st.image(htp, caption= "joe biden", width=350)
+col1.image(htp, caption= "joe biden", width=100)
 
-st.link_button("dmoj link", "https://dmoj.ca/user/laight")
-st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
-st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
+col1.link_button("dmoj link", "https://dmoj.ca/user/laight")
+col2.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
+col2.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
 
