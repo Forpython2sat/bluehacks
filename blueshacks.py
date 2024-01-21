d3f744eb2b31f24b6b775e61c7b38dfd8114554d
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 23:32:55 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -4,15 +4,25 @@ col1, col2 = st.columns([0.5, 0.5])
 
 with st.container():
   with col1:
-    st.header('Interact in a Class Environment')
+    st.header('Lesson 1: School and Teachers')
     image1 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
     st.image(image1)
     st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
+    st.header('Lesson 3: Professionals and Colleagues')
+    image3 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher2.png"
+    st.image(image3)
+    st.link_button("Learn to Interact Smoothly with Peers", "https://bluehacks-3.streamlit.app")
+    
+  
   with col2:
-    st.header('Interact Smoother with Your Peers')
-    image2 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Humaaans%202%20Characters.png"
+    st.header('Lesson 2: Peers')
+    image2 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Humaaans%20Friend%20Meeting.png"
     st.image(image2)
     st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
+    st.header('Lesson 4: Strangers')
+    image3 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Untitled%20(2).png"
+    st.image(image4)
+    st.link_button("Learn to Interact Smoothly with Peers", "https://bluehacks-4.streamlit.app")
     
 st.header('Questionaires and Quizes to Test Your Knowledge')
 st.link_button("Lesson 1 Quiz", "https://1e12b53b-1124-4840-b699-e696e74c47be-00-1tulldaqgf3jp.janeway.replit.dev/")
