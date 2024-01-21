487cb1a58906062729c898fcd40223c4af84e0b6
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 22:05:37 -0500

    Update friendspage.py

--- friendspage.py
+++ friendspage.py
@@ -1,7 +1,6 @@
 import streamlit as st
-omali = false
 
-outl = ['We will first learn how to open conversations. Opening conversations are sometimes quite challenging, and often times, the conversation halts after an exchange of greetings. What would you do to prevent this from happening?', 'Usually, we should talk about something that they are interested in. Psychology shows that humans tend to engage more when something is about them, or if something interests them. Thus, next time you see your friend, ask him whats going good for him, what may be troubling him, and also ask him about his hobbies.']
+outl = ["1. Address them with their appropriate title (i.e. Mr., Ms., Dr., Professor, etc). Understood?", "2. Be polite and use formal language, especially in educational settings. Got that?", "3. Ask questions when you need clarification on a topic. Don't be afraid!", "4. Listen actively and engage in discussions related to the subject. And that is it!", "Good luck communicating!" ]
 
 def counterplus():
     st.session_state.counter += 1
@@ -12,11 +11,14 @@ if "messages" not in st.session_state:
 if 'counter' not in st.session_state:
     st.session_state.counter = -1
 
-with st.chat_message('ai'):
-    st.write('Hello 👋')
-    st.write("Today, we will learn how to effectively communicate with our peers.")
-    st.write("Type something to continue.")
+if 'checker' not in st.session_state:
+    st.session_state.checker = False
 
+with st.chat_message('ai'):
+    st.write('"Hello 👋"')
+    st.write("Today, we will learn how to communicate effectively with a teacher.")
+    st.write("Are you ready to start?")
+    
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
@@ -29,11 +31,11 @@ if prompt:
     if st.session_state.counter >= len(outl):
         response = ('''That is it for this lesson. I hope you learned something!
                     Below is a quiz going through the information you've learned today. Give it a try!''')    
-        omali = True
+        st.session_state.checker = True
     else:
         response = outl[st.session_state.counter]
-        if omali:
-            st.link_button("Lesson 1 Quiz", "https://1e12b53b-1124-4840-b699-e696e74c47be-00-1tulldaqgf3jp.janeway.replit.dev/")
     with st.chat_message('ai'):
         st.markdown(response)
+        if st.session_state.checker:
+            st.link_button("Lesson 1 Quiz", "https://1e12b53b-1124-4840-b699-e696e74c47be-00-1tulldaqgf3jp.janeway.replit.dev/")
     st.session_state.messages.append({'role': 'ai', 'content': response})
