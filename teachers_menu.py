1ff47f94800aae0190c4c040913fb93dccb8e9ee
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-21 10:35:15 -0500

    Update teachers_menu.py

--- teachers_menu.py
+++ teachers_menu.py
@@ -4,9 +4,7 @@ col1, col2, col3, col4 = st.columns([0.25, 0.25, 0.25, 0.25])
 
 with col1:
   with st.container():
-    st.header('Lesson 1: Teachers')
-    image1 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
-    st.image(image1)
+    st.header('Lesson 1: Basics')
     st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
   st.header('Lesson 3: Professionals')
   image3 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher2.png"
@@ -16,11 +14,19 @@ with col1:
   
 with col2:
   with st.container():
-    st.header('Lesson 2: Peers')
-    image2 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Humaaans%20Friend%20Meeting.png"
-    st.image(image2)
+    st.header('Lesson 2: Asking Questions')
     st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
   st.header('Lesson 4: Strangers')
   image4 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Untitled%20(2).png"
   st.image(image4)
   st.link_button("Learn to Talk with Strangers", "https://bluehacks-4.streamlit.app")
+
+with col3:
+  with st.container():
+    st.header("Lesson 3: Email Etiquette")
+    st.link_button("How to compose an effective email", "https://blueshacks2.streamlit.app/")
+
+with col4:
+  with st.container():
+    st.header("Lesson 4: osama")
+    st.link_button("How to compose an effective email", "https://blueshacks2.streamlit.app/")
