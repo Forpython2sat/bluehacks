0c809c6fbc64d46474617f0a809b31161159cb7b
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 19:27:25 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -5,6 +5,8 @@ col1, col2 = st.columns(2)
 with col1:
   st.header('Interact in a Classroom Environment')
   st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
+  image1 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
+  st.image(image1, width = 23)
 with col2:
   st.header('Interact Smoother with Your Peers')
   st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
