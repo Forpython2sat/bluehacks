0cad0877e28be35e9225c05239b2e76d2ccb5cd8
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 23:35:51 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -23,9 +23,3 @@ with st.container():
     image3 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/Untitled%20(2).png"
     st.image(image4)
     st.link_button("Learn to Interact Smoothly with Peers", "https://bluehacks-4.streamlit.app")
-    
-st.header('Questionaires and Quizes to Test Your Knowledge')
-st.link_button("Lesson 1 Quiz", "https://1e12b53b-1124-4840-b699-e696e74c47be-00-1tulldaqgf3jp.janeway.replit.dev/")
-st.link_button("Lesson 2 Quiz", "https://4248fbdf-37ea-423a-964b-16e6fba16303-00-x94itf3kt79e.spock.replit.dev/")
-st.link_button("Lesson 3 Quiz", "https://b407031c-3d95-4e7b-b8db-3eae7d1f240d-00-2tq3v4cwm7zqd.janeway.replit.dev/")
-st.link_button("Lesson 4 Quiz", "https://0f0c1507-87bb-4da5-aedc-4a5e9f795bf1-00-2yb8dchn9mbx1.kirk.replit.dev/")
