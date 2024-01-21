7f17ed8ea9dee3f738d24d14e9239e598c7ba6d0
jfiwe <65134288+jfiwae@users.noreply.github.com>
2024-01-20 19:34:35 -0500

    Update teacherpage.py

--- teacherpage.py
+++ teacherpage.py
@@ -1,9 +1,34 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
+outl = ['Lesson 1: Interacting with a Teacher']
 
-with col1:
-  st.header('Interacting with a Teacher')
-  st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
-with col2:
-  st.header('yrou'ea bad')
+def counterplus():
+    st.session_state.counter += 1
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if 'counter' not in st.session_state:
+    st.session_state.counter = -1
+
+with st.chat_message('ai'):
+    st.write('"Hello 👋"')
+    st.write("Today, we will learn how to effectively communicate with our peers.")
+    st.write("Type something to continue.")
+
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+prompt = st.chat_input(placeholder='Say something', on_submit=counterplus)
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    if st.session_state.counter >= len(outl):
+        response = 'That is it for this lesson. I hope you learned something!'
+    else:
+        response = outl[st.session_state.counter]
+    with st.chat_message('ai'):
+        st.markdown(response)
+    st.session_state.messages.append({'role': 'ai', 'content': response})
