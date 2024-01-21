2b3baf6fad47020171fe0ce91e11b722a659ed6f
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-20 19:25:04 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -4,14 +4,10 @@ col1, col2 = st.columns(2)
 col1.write("this is column 1")
 col2.write("this is column 2")
 
-col1.write("""
-joe biden
-""")
-
-htp="https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
-col1.image(htp, caption= "joe biden", width=100)
-
-col1.link_button("dmoj link", "https://dmoj.ca/user/laight")
-col2.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
-col2.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
+with col1:
+  st.header('Interact in a Classroom Environment')
+  st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
+with col1:
+  st.header('Interact Smoother with Your Peers')
+  st.link_button("Learn to Interact Smoothly with Peers", "https://blueshacks2.streamlit.app/")
 
