a549376d2105e84c4f73a2fc529e1aec2d4c48d6
Forpython2sat <68508045+Forpython2sat@users.noreply.github.com>
2024-01-21 09:38:55 -0500

    Update blueshacks.py

--- blueshacks.py
+++ blueshacks.py
@@ -4,11 +4,11 @@ col1, col2 = st.columns([0.5, 0.5])
 
 with col1:
   with st.container():
-    st.header('Lesson 1: School and Teachers')
+    st.header('Lesson 1: Teachers')
     image1 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher.png"
     st.image(image1)
     st.link_button("Learn to Interact in a Classroom Environment", "https://blueshackst.streamlit.app/")
-  st.header('Lesson 3: Professionals and Colleagues')
+  st.header('Lesson 3: Professionals')
   image3 = "https://raw.githubusercontent.com/Forpython2sat/bluehacks/main/art/teacher2.png"
   st.image(image3)
   st.link_button("Learn to Interact Smoothly with Peers", "https://bluehacks-3.streamlit.app")
